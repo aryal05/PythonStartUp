489569eba979016c7c7b58b4e1892d316de37dec
Ami <aryal.rajat05@gmail.com>
2024-06-08 20:31:09 +0545

    Python

---
+++ First.py
@@ -0,0 +1,74 @@
+name = "Appna college"
+age = 20
+price = 59.99
+address = "Kathmandu"
+isPass = True
+
+print(type(name))
+print(type(age))
+print(type(price))
+print(type(isPass))
+# x, y, z = "Orange", "Banana", "Cherry"
+# print(x)
+# print(y)
+# print(z)
+
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
+
+# Printing Sum Of Two Numbers
+
+# a = 2
+# b = 9
+# sum = a+b
+# print(sum)
+
+# print(a ** b) # This is for a^b
+
+val1 = True
+val2 = False
+print("AND OPERATOR:", val1 and val2) # Both the value should be the True value to return True.
+print("OR OPERATOR:", val1 or val2) # Even if one of the value is True and The other one is False then also it returns True.
+
+# Type Conversion(Automatically) and Type Casting(it is done manually)
+
+
+#Type conversion
+val3 = 4
+val4 = 12.36
+print(val3 + val4)
+
+
+#Casting Conversion
+a = int("2")
+b = 15
+print(a+b)
+
+#Input
+# name = input("Enter Your Name:")
+# age = int(input("Enter Your Age:"))
+# marks = float(input("Ënter Your Marks:"))
+
+# print("Welcome", name)
+# print("age = ",age)
+# print("marks = ",marks)
+
+num1 =int(input("Enter Your First Number:"))
+num2 =int(input("Enter your second Number:"))
+print("Sum = ",num1 + num2)
+
+
+#Wap to input side if a square & print its area
+
+side = float(input("Enter square sides:"))
+print ("area = ", side * side)
+
+# Wap to input 2 floating point number & print their average
+
+int1 = int(input("First Floating value:"))
+int2 = int(input("Second Floating value:"))
+Sum = int1 + int2
+ 
+print("Average = ", Sum/2 )
