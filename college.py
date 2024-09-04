6620c796d8e6ff28847dbc4e4d60779d9735cf3f
aryal05 <aryal.rajat05@gmail.com>
2024-09-04 19:20:40 +0545

    function in python

---
+++ college.py
@@ -0,0 +1,12 @@
+print("Pranjal")
+pi = 3.14
+radius = int(input("Enter R: "))
+areaOfCircle = pi * radius**2
+print("areaOfCircle = ", areaOfCircle)
+
+physics = int(input("Enter the physics Marks:"))
+maths = int(input("Enter the Maths Marks:"))
+computer = int(input("Enter the Computer Marks:"))
+totalMarks = physics + maths + computer
+percentage = (totalMarks)/300*100
+print("percentage is = ",percentage)
