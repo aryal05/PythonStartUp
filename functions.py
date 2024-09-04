89a8ecf71fab3d0ff138e4ee75eb0e6e3a63f82d
aryal05 <aryal.rajat05@gmail.com>
2024-09-04 19:34:50 +0545

    function to calculate

--- functions.py
+++ functions.py
@@ -1,6 +1,12 @@
 #Block of Statement that performs a specific task
 
-a=5;
-b=10;
-sum = a+b
-print(sum)
+# a=5;
+# b=10;
+# sum = a+b
+# print(sum)
+
+def calculate(a,b):
+    sum = a +b
+    print(sum)
+    return sum
+calculate(45,34) #This is the argument
