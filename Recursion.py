4b74542fc4eea8c3761040dd512325e9c1fb0c06
aryal05 <aryal.rajat05@gmail.com>
2024-09-06 20:48:17 +0545

    questions implementation

--- Recursion.py
+++ Recursion.py
@@ -18,7 +18,7 @@ def fact(n):
       return n * fact(n-1)
    
 print(fact(4))   
-
+# Calculate sum using recursion
 def sum(n):
    if(n == 0):
       return 0
@@ -26,7 +26,7 @@ def sum(n):
    return sum(n-1) + n
 print(sum(12))
 
-
+# print list of items using recursion
 list = ["car","bike","truck", "plane"]
 
 def lis(lists,index=0):
@@ -35,3 +35,14 @@ def lis(lists,index=0):
    print(lists[index])
    lis(lists,index+1)
 print(lis(list))
+
+
+
+# def hg(n):
+#    if(n ==1):
+#       return
+#    print(n)
+#    hg(n-1)
+# hg(23)
+
+
