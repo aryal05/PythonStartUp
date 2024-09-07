bed8214e082da67a328fefca842fc99adcc12f23
aryal05 <aryal.rajat05@gmail.com>
2024-09-07 20:15:13 +0545

    write method in py

--- Files_Input_Output.py
+++ Files_Input_Output.py
@@ -5,14 +5,21 @@
 
 #Binary Files (.mp4, .jpg, .exe etc)
 
-f = open("demo.txt", "r")
-data = f.read()
-print(data)
-print(type(data))
-f.close()
+# f = open("demo.txt", "r")
+# data = f.read()
+# print(data)
+# print(type(data))
+# f.close()
 
 
-f = open("demo.txt", "r")
-line = f.readline()
-print(line)
-f.close()
+# f = open("demo.txt", "r")
+# line = f.readline()
+# print(line)
+# f.close()
+
+
+#Writing in file
+
+write = open("demo.txt", "w")
+write.write("This is the changed file due to writing method")
+write.close()
