af55f8de1f83355ce32b695794cf42aa5afa6cce
aryal05 <aryal.rajat05@gmail.com>
2024-09-07 20:17:50 +0545

    append the writing in file

--- Files_Input_Output.py
+++ Files_Input_Output.py
@@ -5,21 +5,29 @@
 
 #Binary Files (.mp4, .jpg, .exe etc)
 
-# f = open("demo.txt", "r")
-# data = f.read()
-# print(data)
-# print(type(data))
-# f.close()
+f = open("demo.txt", "r")
+data = f.read()
+print(data)
+print(type(data))
+f.close()
 
 
-# f = open("demo.txt", "r")
-# line = f.readline()
-# print(line)
-# f.close()
+f = open("demo.txt", "r")
+line = f.readline()
+print(line)
+f.close()
 
 
 #Writing in file
 
 write = open("demo.txt", "w")
-write.write("This is the changed file due to writing method")
+write.write("This is the changed file due to writing method") # This changes entire text of demo.txt file and add write.write(" this line to the existing text.")
 write.close()
+
+
+
+# Writing in file using append
+append = open("demo.txt", "a")
+append.write(" This is appended text in the existing file.")
+
+append.close()
