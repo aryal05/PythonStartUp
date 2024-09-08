14b8ab3d65de6f49ec01ac4b45e859be702c4e56
aryal05 <aryal.rajat05@gmail.com>
2024-09-08 11:23:36 +0545

    creating class and object

--- OOPS.py
+++ OOPS.py
@@ -1,2 +1,15 @@
 #To Map with real world scenarios, we started using objects in code which is called Object-Oriented Programming (OOP).
 
+#Class And Object in Python
+
+#Class is a blueprint for creating an object. It defines the characteristics and behavior of an object.
+
+#Creating Class
+class Car:
+    name = "lambo"
+    color = "red" 
+    speed = 200
+
+#Creating Object
+my_car = Car()
+print(my_car)
